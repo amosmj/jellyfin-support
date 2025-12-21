d335e1aa466517be39322ac20fa816e7f141154e
amosmj <amosmj@gmail.com>
2025-12-21 15:50:02 -0600

    pip installed parmiko becaue the internet and copilot are saying thats the best way to ssh to the server.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+import paramiko
+
 my_log = logging.getLogger('jellyfin-support')
 logging.basicConfig(filename='jellyfin-support.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -14,7 +16,11 @@ def log_me(passing_function):
 
 def make_list_of_dirs(dir: str):
     raise NotImplementedError
+
 def make_list_of_files(list_of_dirs: list[str]):
+    raise NotImplementedError
+
+def mofify_string_with_pattern(string_in: str, substring_to_mod: str, mod_to: str ):
     raise NotImplementedError
 
 def rename_file(old_name, new_name):

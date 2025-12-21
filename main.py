39c2a30c534dadfab07af95fb788435df56008fc
amosmj <amosmj@gmail.com>
2025-12-21 16:09:24 -0600

    added tk interface but getting two instances. Need to debug

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 import paramiko
 import tkinter as tk
+from tkinter import filedialog
 
 my_log = logging.getLogger('jellyfin-support')
 logging.basicConfig(filename='jellyfin-support.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -15,8 +16,13 @@ def log_me(passing_function):
         return passing_function(*args, **kwargs)
     return logger
 
-def have_user_designate_folder():
-    raise NotImplementedError
+@log_me
+def have_user_designate_folder()-> str:
+    root = tk.Tk()
+    root.withdraw()
+    dirname = filedialog.askdirectory()
+    my_log.info(f"Directory was selected as {dirname}")
+    return dirname
 
 def make_list_of_dirs(dir: str):
     raise NotImplementedError
@@ -40,5 +46,5 @@ def rename_file(old_name, new_name):
 
 if __name__ =="__main__":
     my_log.info("main.py started")
-
+    root_folder = have_user_designate_folder()
     my_log.info("main.py ended")

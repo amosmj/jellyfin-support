91cdbc80f6c3d9050a49bec2a2067b84c66c4450
amosmj <amosmj@gmail.com>
2026-01-18 04:56:07 -0600

    save some changes to shows list and started working on a new function that renames movie files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import os
-
+import pathlib
 import paramiko
 import tkinter as tk
 from tkinter import filedialog
@@ -24,8 +24,7 @@ def have_user_designate_folder()-> str:
     my_log.info(f"Directory was selected as {dirname}")
     return dirname
 
-def make_list_of_dirs(dir: str):
-    raise NotImplementedError
+
 
 def make_list_of_files(list_of_dirs: list[str]):
     raise NotImplementedError
@@ -33,16 +32,36 @@ def make_list_of_files(list_of_dirs: list[str]):
 def mofify_string_with_pattern(string_in: str, substring_to_mod: str, mod_to: str ):
     raise NotImplementedError
 
-def rename_file(old_name, new_name):
-    try:
-        os.rename(old_name, new_name)
-        logging.info(f"File renamed from {old_name} to {new_name}")
-    except FileNotFoundError:
-        logging.error(f"Error: The file {old_name} does not exist.")
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-    return True
+def regroup_files_in_folders(out_dir: str, list_of_in_dir: list[str]):
+    raise NotImplementedError
 
+def simple_string_replacement(substring_to_replace: str, replace_with: str, list_of_files: list[str], error_when_not_found= False)->list[str]:
+    SUBSTRING_NOT_FOUND = -1
+    list_of_files_out = []
+    for file in list_of_files:
+        if file.find(substring_to_replace) != SUBSTRING_NOT_FOUND:
+            file.replace(substring_to_replace,replace_with)
+        else:
+            if error_when_not_found:
+                message = (f"The substring {substring_to_replace} was not found in {file}. Per your request, an error has occurred")
+                raise ValueError
+        list_of_files_out.append(file)
+    return list_of_files_out
+                
+
+def rename_mkv_files_to_match_directories(root_directory: str, file_type: str|None = None):
+    dir_list = []
+    while len(dir_list) < 1{}
+        root_path = have_user_designate_folder()
+
+    ## Look in directory for files and directories
+    ## for files
+        ## if file_type matches or file_type = None
+        ## provide feedback
+    ## for directories
+        ##r repeat this process
+
+    raise NotImplementedError
 
 if __name__ =="__main__":
     my_log.info("main.py started")
